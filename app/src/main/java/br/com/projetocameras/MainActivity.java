d04761d14de58e9cb65b6790f8f4947b850e6db2
felipedofd <felipedofd@hotmail.com>
2021-07-22 11:51:27 -0300

    substituição da chamada da view findViewById por binding

--- app/src/main/java/br/com/projetocameras/MainActivity.java
+++ app/src/main/java/br/com/projetocameras/MainActivity.java
@@ -1,7 +1,6 @@
 package br.com.projetocameras;
 
 import android.os.Bundle;
-import android.webkit.WebView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -18,8 +17,6 @@ import br.com.projetocameras.databinding.ActivityMainBinding;
 
 
 public class MainActivity extends AppCompatActivity {
-//variavel do tipo webview
-    private WebView webView;
 
     private ActivityMainBinding binding;
 
@@ -27,12 +24,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.acativity_main);
-
-        webView = findViewById(R.id.cam01);
-        webView.loadUrl("http://10.0.0.197:81/videostream.cgi?loginuse=admin&loginpas=");
-
-
 
     }
 
