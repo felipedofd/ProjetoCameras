b3890b346ca30e012802b62f2f336345d6506a53
felipedofd <felipedofd@hotmail.com>
2021-07-23 18:26:56 -0300

    imports

---
+++ app/src/main/java/br/com/projetocameras/CamAdapter.java
@@ -0,0 +1,12 @@
+package br.com.projetocameras;
+
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+import br.com.projetocameras.databinding.ItemCamBinding;
+
